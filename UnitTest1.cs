526df3998dbbd387558d97b3c58caee20779c7b7
SteveStrong <sstrong@apprenticesystems.com>
2020-12-01 19:59:25 -0500

    call to tests

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,5 +14,29 @@ namespace FoundryCoreXUnit
 
             Assert.Equal(100, prop2a.Value);
         }
+
+
+        
+        [Fact]
+       public void Test2()
+        {
+            var prop = FoPropertyBuilder.Start()
+                .SetName("volume");
+
+            var build = FoComponentBuilder.Start()
+                .SetName("Box")
+                .AddProperty<string>("label")
+                .AddProperty<int>("tag", 100)
+                .AddProperty<double>("width")
+                .AddProperty<double>("depth")
+                .AddProperty<double>("height")
+                
+                .AddProperty(prop.Build<double>())
+                .AddProperty(prop.SetName("title").Build<string>());
+
+            var result1 = build.Build();
+            Console.WriteLine($"{result1.AsJson()}");
+            Assert.Equal(result1, result1);
+        }
     }
 }
